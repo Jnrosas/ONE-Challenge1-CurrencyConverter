7928ccf14f989747dc5209b895f98e59e454f4ef
Juan Rosas <rosas.juan@live.com>
2024-04-15 21:46:30 -0300

    added date and time

--- src/models/ConversionHistory.java
+++ src/models/ConversionHistory.java
@@ -2,12 +2,22 @@ package models;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ConversionHistory {
 
     public void saveConversions(String conversion) throws IOException {
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("***** EEEE, dd MMMM yyyy, hh:mm:ss *****");
+        String text = dateTime.format(formatter);
+
         FileWriter savedConversion = new FileWriter("history.txt", true);
+        savedConversion.write(text);
         savedConversion.write(String.valueOf(conversion));
+        savedConversion.write("\n");
         savedConversion.close();
     }
 }
