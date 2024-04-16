5f1d81aceb8e45bf21ead0cf1bf9518ffec07007
Juan Rosas <rosas.juan@live.com>
2024-04-15 22:41:44 -0300

    fixed date time format for history

--- src/models/ConversionHistory.java
+++ src/models/ConversionHistory.java
@@ -11,13 +11,14 @@ public class ConversionHistory {
     public void saveConversions(String conversion) throws IOException {
 
         LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("***** EEEE, dd MMMM yyyy, hh:mm:ss *****");
+        DateTimeFormatter formatter = DateTimeFormatter.
+                ofPattern("      EEEE, dd MMMM yyyy - HH:mm:ss      ");
         String text = dateTime.format(formatter);
 
         FileWriter savedConversion = new FileWriter("history.txt", true);
         savedConversion.write(text);
         savedConversion.write(String.valueOf(conversion));
-        savedConversion.write("\n");
+        savedConversion.write("\n\n");
         savedConversion.close();
     }
 }
